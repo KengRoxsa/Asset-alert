ba35b12bd839eb0e1c06caba955560c0340d4a01
Keng <niti.unyapho@gmail.com>
2025-08-28 07:50:33 +0700

    fix spatacuz

--- src/lib/fetchGoldServer.js
+++ src/lib/fetchGoldServer.js
@@ -1,20 +1,18 @@
-"use server";
 import chromium from "@sparticuz/chromium";
+
 import puppeteer from "puppeteer-core";
 
 export async function fetchGoldPrice() {
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
+    executablePath: await chromium.executablePath(), // จะดาวน์โหลด binary ตอน build
     headless: chromium.headless,
   });
 
   try {
     const page = await browser.newPage();
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-    );
+
     await page.goto(process.env.GOLD_PRICE_URL, { waitUntil: "networkidle2" });
 
     await page.waitForFunction(() => {
