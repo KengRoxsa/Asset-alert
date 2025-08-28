b910359f31513797569825c457eada6a3f95cb96
Keng <niti.unyapho@gmail.com>
2025-08-28 08:07:12 +0700

    gold still doesnt exit

--- src/lib/fetchGoldServer.js
+++ src/lib/fetchGoldServer.js
@@ -1,20 +1,21 @@
+"use server";
+import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
 
-import puppeteer from "puppeteer-core";
-
 export async function fetchGoldPrice() {
+  // Launch Chromium ผ่าน sparticuz
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(), // จะดาวน์โหลด binary ตอน build
+    executablePath: await chromium.executablePath(), // จะดาวน์โหลด binary ให้เอง
     headless: chromium.headless,
   });
 
   try {
     const page = await browser.newPage();
-
     await page.goto(process.env.GOLD_PRICE_URL, { waitUntil: "networkidle2" });
 
+    // รอให้ element ราคาปรากฏ
     await page.waitForFunction(() => {
       const el = document.querySelector("span.last-change.ng-binding");
       return el && el.textContent.trim() !== "";
